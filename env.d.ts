33912694c58bd981c632237795e03873abcd03a1
Anna <anna.fokidou@gmail.com>
2025-05-19 10:54:45 +0200

    Προσθήκη typings αρχείου env.d.ts για Supabase μεταβλητές

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,13 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly SUPABASE_URL: string;
-  readonly SUPABASE_SERVICE_ROLE_KEY: string;
-  // μπορείς να προσθέσεις κι άλλες εδώ αν χρειαστεί
+  readonly SUPABASE_URL: string
+  readonly SUPABASE_SERVICE_ROLE_KEY: string
+
+  readonly PUBLIC_SUPABASE_URL: string
+  readonly PUBLIC_SUPABASE_ANON_KEY: string
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv
 }
