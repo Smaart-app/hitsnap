0104afceb8217c60fd67a6ab1667ddc4cbfd21c9
Anna <anna.fokidou@gmail.com>
2025-05-22 08:19:44 +0200

    Προστέθηκε Netlify adapter στο astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
+  adapter: netlify(),
   integrations: [tailwind(), mdx(), react()],
 });
