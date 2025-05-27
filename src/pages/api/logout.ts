7c3d1aa2b24aab4fa359947240fb014e61cceb63
Anna <anna.fokidou@gmail.com>
2025-05-27 09:45:48 +0200

    commit & push

---
+++ src/pages/api/logout.ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async ({ cookies }) => {
+  cookies.delete('sb-access-token', { path: '/' });
+  cookies.delete('sb-refresh-token', { path: '/' });
+
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: '/login'
+    }
+  });
+};
