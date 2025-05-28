1148edd50649201e0efde833dd01b2adabcf3779
Anna <anna.fokidou@gmail.com>
2025-05-28 14:25:02 +0200

    commit & push

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import netlify from "@astrojs/netlify/functions"; // ✅ ΣΩΣΤΟΣ adapter για output: 'server'
+import netlify from "@astrojs/netlify/functions";
 
 export default defineConfig({
   output: "server",
