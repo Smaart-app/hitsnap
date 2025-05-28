020a0ee1e2791e5e5aee5a3b645804fc13a00e44
Anna <anna.fokidou@gmail.com>
2025-05-28 17:47:32 +0200

    commit & push

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,8 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   adapter: netlify(),
   integrations: [tailwind(), mdx(), react()],
 });
