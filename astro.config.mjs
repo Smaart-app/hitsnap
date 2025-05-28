1ba7ec8aaae3c8a019fc9004c5f1b45167b50e4d
Anna <anna.fokidou@gmail.com>
2025-05-28 14:59:18 +0200

    commit & push

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import staticAdapter from "@astrojs/adapter-static";
 
 export default defineConfig({
   output: "static",
+  adapter: staticAdapter(),
   integrations: [tailwind(), mdx(), react()],
 });
