493acd1006038c90cb52b91973808bfc1121848a
Anna <anna.fokidou@gmail.com>
2025-06-07 21:13:10 +0200

    astro.config.mjs, package-lock.json, package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions"; // ✅ Εδώ αλλάζει!
 
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(), // ✅ Εδώ αλλάζει!
   integrations: [tailwind(), mdx(), react()],
 });
